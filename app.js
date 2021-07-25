b93d90ce2b99ff732ad15280d752f05471428df0
GeonNamGung <rndrjs123@naver.com>
2021-07-25 19:02:21 +0900

    hide detail message when response code is 500
    only do logging

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const router = require('./routes/index');
+const router = require("./routes/index");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', router);
+app.use("/", router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,7 +24,18 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   console.error(err);
-  res.status(err.status || 500).json(err.message);
+  const statusCode = err.status || 500;
+  res
+    .status(statusCode)
+    .json({
+      message: statusCode === 500 ? "Server error" : err.message
+    })
+    .end();
+});
+
+//catch unCaughtException
+process.on("uncaughtException", function(err) {
+  console.error("uncaughtException (Node is alive)", err);
 });
 
 module.exports = app;
